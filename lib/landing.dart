af993237e0af5a6913232107ef792c0c48a5ce46
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-05-12 01:39:38 +0530

    merge authentication branch

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myward/models/nursing_staff.dart';
 import 'package:myward/screens/home/home.dart';
-import 'package:myward/screens/login/login1.dart';
+import 'package:myward/screens/login/login_view.dart';
 import 'package:provider/provider.dart';
 
 class LandingPage extends StatelessWidget {
